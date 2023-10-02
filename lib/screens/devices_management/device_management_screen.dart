ae91ba771208ef8bf247e542d75f0b5555f7c29b
calinvasileandrei <calinvasileandrei@gmail.com>
2023-10-03 01:22:43 +0200

    better socket server with hooks

--- lib/screens/devices_management/device_management_screen.dart
+++ lib/screens/devices_management/device_management_screen.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:socket_io/socket_io.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vr_trip/services/socket_server/socket_server_service.dart';
 import 'package:vr_trip/shared/socket_chat/socket_chat.dart';
 
 final devicesProvider = Provider<int>((ref) => 0);
 
 enum DeviceType { advertiser, browser }
 
-class DeviceManagementScreen extends HookWidget {
+class DeviceManagementScreen extends HookConsumerWidget {
   const DeviceManagementScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final socketServer = useMemoized(() => SocketServerService.instance);
     final devices = useState<List<String>>([]);
     final messages = useState<List<String>>([]);
+    final isActive = ref.watch(socketServerProvider);
 
     void addMessage(String value) {
       final currentList = List<String>.from(messages.value);
@@ -28,54 +31,34 @@ class DeviceManagementScreen extends HookWidget {
       devices.value = currentList;
     }
 
-    void removeItem(String name){
+    void removeItem(String name) {
       final currentList = List<String>.from(devices.value);
       currentList.remove(name);
       devices.value = currentList;
     }
 
-    final socketServer = useMemoized(() => Server());
-    void startSocketServer() {
-      socketServer.listen(3000); // You can choose any available port
-      print('Socket server started on port 3000');
-    }
-
     useEffect(() {
-      socketServer.on('connection', (socket) {
-        print('Client connected: ${socket.id}');
-        addItem(socket.id);
-
-        socket.on('message', (data) {
-          print('Received message: $data');
-          socket.emit('message', 'Server received your message: $data');
-          addMessage(data);
-        });
-
-        socket.on('disconnect', (_) {
-          print('Client disconnected: ${socket.id}');
-          removeItem(socket.id);
-        });
-
-        return () {
-          socket.disconnect();
-        };
-      });
+      socketServer.startSocketServer(ref);
 
       return () {
-        socketServer.close();
+        socketServer.stopSocketServer(ref);
       };
     }, const []);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Device Manager'),
       ),
-      body: Center(child: Column(
+      body: Center(
+          child: Column(
         children: [
+          Text(
+              'Socket server : ${isActive ? 'active' : 'stopped'}'),
           ElevatedButton(
             onPressed: () {
-              startSocketServer();
+              socketServer.stopSocketServer(ref);
             },
-            child: Text('Start Socket Server'),
+            child: Text('Stop server'),
           ),
           SocketChat(items: devices.value),
           SocketChat(items: messages.value),
--- lib/services/socket_server/socket_server_service.dart
+++ lib/services/socket_server/socket_server_service.dart
@@ -1,4 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:socket_io/socket_io.dart';
+
+final socketServerProvider = NotifierProvider<SocketServer, bool>(SocketServer.new);
+
+class SocketServer extends Notifier<bool> {
+  @override
+  bool build() => true;
+
+  void setServer( bool value ){
+    state = value;
+  }
+}
 
 class SocketServerService {
   static SocketServerService? _instance;
@@ -13,11 +25,14 @@ class SocketServerService {
     return _instance!;
   }
 
-  void startSocketServer() {
+  void startSocketServer(WidgetRef ref) {
     if (_socketServer == null) {
       _socketServer = Server();
       _socketServer!.listen(3000); // You can choose any available port
       print('Socket server started on port 3000');
+
+      // Notify listeners that the server is running
+      //ref.read(socketServerProvider.notifier).setServer(true); // Update server status
 
       _socketServer!.on('connection', (socket) {
         print('Client connected: ${socket.id}');
@@ -26,11 +41,12 @@ class SocketServerService {
     }
   }
 
-  void stopSocketServer() {
+  void stopSocketServer(WidgetRef ref) {
     if (_socketServer != null) {
       _socketServer!.close();
       print('Socket server stopped');
       _socketServer = null; // Reset the instance
+      ref.read(socketServerProvider.notifier).setServer(false); // Update server status
     }
   }
 }
