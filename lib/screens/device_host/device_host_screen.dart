bf53981d3d3693dbc4e014fc053f72fee61325da
calinvasileandrei <calinvasileandrei@gmail.com>
2023-10-11 17:28:02 +0200

    refactor methods

--- lib/screens/device_host/device_host_screen.dart
+++ lib/screens/device_host/device_host_screen.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vr_trip/screens/device_host/widgets/device_host_socket/device_host_socket.dart';
+import 'package:vr_trip/screens/vr_player/vr_player_screen.dart';
 import 'package:vr_trip/services/network_discovery_client/network_discovery_client.dart';
 
 class DeviceHostScreen extends HookConsumerWidget {
   const DeviceHostScreen({super.key});
 
+  void navigateWithVideo(BuildContext context, String videoPath) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VrPlayerScreen(videoPath: videoPath),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final serverIp = ref.watch(networkDiscoveryClientServerIpProvider);
 
-    renderDiscoveryOrDeviceHostSocket() {
+    Widget renderDiscoveryOrDeviceHostSocket() {
       if (serverIp == null) {
         return const Center(child: Text('Discovery is running'));
       }
-      return DeviceHostSocket(serverIp: serverIp);
+      return DeviceHostSocket(
+          serverIp: serverIp,
+          navigateToVrPlayer: (videoPath) =>
+              navigateWithVideo(context, videoPath));
     }
 
     return Scaffold(
--- lib/screens/device_host/widgets/device_host_socket/device_host_socket.dart
+++ lib/screens/device_host/widgets/device_host_socket/device_host_socket.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vr_trip/models/socket_protocol_message.dart';
-import 'package:vr_trip/screens/vr_player/vr_player_screen.dart';
 import 'package:vr_trip/services/network_discovery_client/network_discovery_client_provider.dart';
 import 'package:vr_trip/services/socket_protocol/socket_protocol_service.dart';
 import 'package:vr_trip/shared/socket_chat/socket_chat.dart';
@@ -9,32 +9,37 @@ import 'package:vr_trip/utils/logger.dart';
 
 class DeviceHostSocket extends HookConsumerWidget {
   final String serverIp;
+  final Function(String) navigateToVrPlayer;
 
-  const DeviceHostSocket({super.key, required this.serverIp});
+  const DeviceHostSocket(
+      {super.key, required this.serverIp, required this.navigateToVrPlayer});
+
+  void getLastMessage(BuildContext context, List<String> messages) async {
+    Logger.log('getLastMessage - messages: $messages');
+    if (messages.isEmpty) return;
+
+    final lastMessageIndex = messages.length - 1;
+    final message = messages[lastMessageIndex];
+    SocketAction action = SocketProtocolService.parseMessage(message);
+
+    navigateToVrPlayer(action.value);
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final messages = ref.watch(hostMessagesSP(serverIp));
+    // If it's in the 'data' state, get the value.
+    final List<String> messagesList =
+        messages.maybeWhen(data: (value) => value, orElse: () => []);
 
-    navigateWithVideo(String videoPath) {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => VrPlayerScreen(
-                    videoPath: videoPath,
-                  )));
-    }
-
-    getLastMessage(List<String> messages) async {
-      Logger.log('getLastMessage - messages: $messages');
-      if (messages.isEmpty) return;
-
-      final lastMessageIndex = messages.length - 1;
-      final message = messages[lastMessageIndex];
-      SocketAction action = SocketProtocolService.parseMessage(message);
-
-      navigateWithVideo(action.value);
-    }
+    useEffect(() {
+      Logger.log('useEffect - messagesList: $messagesList');
+      if (messagesList.isNotEmpty) {
+        // Assuming you have a function named 'handleListChange' that you want to execute:
+        getLastMessage(context, messagesList);
+      }
+      return null;
+    }, [messagesList.length]);
 
     return Center(
         child: Column(
@@ -45,9 +50,6 @@ class DeviceHostSocket extends HookConsumerWidget {
             loading: () => const Text('Awaiting messages from server...'),
             error: (error, stackTrace) => Text(error.toString()),
             data: (messagesItems) {
-              getLastMessage(messagesItems);
-
-              // Display all the messages in a scrollable list view.
               return SocketChat(items: messagesItems);
             },
           ),
